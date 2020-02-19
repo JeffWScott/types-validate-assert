380a3807197444c9cd076f7bb2cb56dd8ec69558
JeffWScott <jeffscott@outlook.com>
2020-02-19 09:50:05 -0500

    updated gulp rollup settings

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 
 gulp.task('build', () => {
-  return gulp.src('src/*.js')
+  return gulp.src('src/index.js')
     .pipe(rollup({ plugins: [babel(), resolve(), commonjs()] }, 'umd'))
     .pipe(gulp.dest('dist/'));
 });
